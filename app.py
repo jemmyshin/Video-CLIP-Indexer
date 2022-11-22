266e89e7f5dee0915c3e56a2a5a3d5c02ebac6e0
jemmyshin <jem.fu@jina.ai>
2022-11-22 19:52:26 +0800

    feat: add av dependency

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import numpy as np
 import streamlit as st
-from helper import search_frame
+from helper import search_frame, get_keyframes_data
 import os
+os.system('sudo apt-get install --no-upgrade ffmpeg')
+import skvideo.io
 import shutil
 import av
 from docarray import DocumentArray, Document
@@ -14,7 +17,7 @@ topn_value = st.text_input('Top N', '5')
 num_frames = st.text_input('Num_frames', '50')
 cut_sim_value = st.text_input('Cut Sim', '0.6')
 cas_url = st.text_input('CLIP-as-service Server', 'grpcs://api.clip.jina.ai:2096')
-token = st.text_input('Token', '')
+token = st.text_input('Token', '31454a8d0823445012c6de5623aed215')
 analysis_button = st.button('Extract Key Frames')
 search_button = st.button('Search')
 
@@ -25,42 +28,60 @@ if analysis_button:
         with open('tmp_videos/' + uploaded_file.name, 'wb') as f:
             f.write(uploaded_file.getvalue())
 
-        keyframe_data = []
-        with av.open('tmp_videos/' + uploaded_file.name) as container:
-            # Signal that we only want to look at keyframes.
-            stream = container.streams.video[0]
-            stream.codec_context.skip_frame = 'NONKEY'
-            for frame in container.decode(stream):
-                frame.to_image().save(
-                    'tmp_key_frames/frame.{:04d}.png'.format(frame.pts),
-                    quality=80,
-                )
-                keyframe_data.append('tmp_key_frames/frame.{:04d}.png'.format(frame.pts))
-
-        # st.session_state.video_data = skvideo.io.vread('tmp_videos/' + uploaded_file.name, num_frames=num_frames)
-        # shutil.rmtree('tmp_videos')
-        # keyframe_data = get_keyframes_data(st.session_state.video_data, float(cut_sim_value))
-        st.session_state.keyframe_data = DocumentArray([Document(id=tup.split('/')[1],
-                                                                 uri=tup).load_uri_to_blob() for tup in keyframe_data])
-
-        # for d in st.session_state.keyframe_data:
-        #     d.tensor = None
-        shutil.rmtree('tmp_videos')
-        shutil.rmtree('tmp_key_frames')
+        video_data = skvideo.io.vread('tmp_videos/' + uploaded_file.name)
+        keyframe_data = get_keyframes_data(video_data, float(cut_sim_value))
+        spams, ndarray, scores = search_frame(keyframe_data, text_prompt,
+                                              int(topn_value), cas_url, token)
+        for spam in spams:
+            i = spams.index(spam)
+            save_name = 'tmp_videos/' + str(i) + '_tmp.mp4'
+            skvideo.io.vwrite(save_name,
+                              video_data[int(spam['left']):int(spam['right'])])
+            st.video(save_name)
+            os.remove(save_name)
         st.success('Done extracting key frames, now you can search for it!')
+        # keyframe_data = []
+        # with av.open('tmp_videos/' + uploaded_file.name) as container:
+        #     # Signal that we only want to look at keyframes.
+        #     stream = container.streams.video[0]
+        #     stream.codec_context.skip_frame = 'NONKEY'
+        #     for frame in container.decode(stream):
+        #         frame.to_image().save(
+        #             'tmp_key_frames/frame.{:04d}.png'.format(frame.pts),
+        #             quality=80,
+        #         )
+        #         keyframe_data.append('tmp_key_frames/frame.{:04d}.png'.format(frame.pts))
+        # keyframe_data = keyframe_data[:5]
+        # st.session_state.keyframe_data = DocumentArray([Document(id=tup.split('/')[1],
+        #                                                          uri=tup).load_uri_to_blob() for tup in keyframe_data])
+        # for d in st.session_state.keyframe_data:
+        #     d.uri = None
+        # shutil.rmtree('tmp_videos')
+        # # shutil.rmtree('tmp_key_frames')
+        # st.success('Done extracting key frames, now you can search for it!')
 
 if search_button:
     if 'keyframe_data' in st.session_state:
         with st.spinner(f"We are searching from {len(st.session_state.keyframe_data)} frames..."):
-            spams, ndarray, scores = search_frame(st.session_state.keyframe_data, text_prompt, int(topn_value), cas_url, token)
-            for spam in spams:
-                i = spams.index(spam)
-                os.makedirs('tmp_videos', exist_ok=True)
-                save_name = 'tmp_videos/' + str(i) + '_tmp.mp4'
-                skvideo.io.vwrite(save_name, st.session_state.video_data[int(spam['left']):int(spam['right'])])
-                st.video(save_name)
-                os.remove(save_name)
-                shutil.rmtree('tmp_videos')
+            results = search_frame(st.session_state.keyframe_data, text_prompt, int(topn_value), cas_url, token)
+            results = DocumentArray([doc.convert_blob_to_image_tensor() for doc in results])
+            img = np.array([r.tensor for r in results])
+
+
+
+            # os.makedirs('tmp_videos', exist_ok=True)
+            # save_name = 'tmp_videos/tmp.mp4'
+            #
+            # container = av.open(save_name, 'w')
+            # for im in img:
+            #     frame = av.VideoFrame.from_ndarray(im, format="rgb24")
+            #     for packet in stream.encode(frame):
+            #         container.mux(packet)
+            #
+            # st.video(save_name)
+            # os.remove(save_name)
+            # shutil.rmtree('tmp_videos')
+
         st.success('Done!')
     else:
         st.warning('Please extract the key frame first')
--- helper.py
+++ helper.py
@@ -1,42 +1,42 @@
+import numpy as np
+
 from clip_client import Client
 from docarray import Document, DocumentArray
-# import imagehash
-# from PIL import Image
-# import numpy as np
-# import os
+import imagehash
+from PIL import Image
+import numpy as np
 
 
-# keyframe_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'keyframes')
-# if not os.path.exists(keyframe_path):
-#     os.mkdir(keyframe_path)
-
-
-# def get_keyframes_data(video_data: 'np.ndarray', cut_sim: float):
-#     last_hash = imagehash.phash(Image.fromarray(video_data[0]))
-#     key_frames = [0]
-#     frame_num = 0
-#     for each_frame in video_data:
-#         frame_hash = imagehash.phash(Image.fromarray(each_frame))
-#         similarity = 1 - (last_hash - frame_hash) / len(frame_hash.hash) ** 2
-#         if similarity < cut_sim:
-#             key_frames.append(frame_num)
-#         frame_num += 1
-#         last_hash = frame_hash
-#     video_length = len(video_data)
-#     key_frames.append(video_length)
-#     keyframes_data = [((i, key_frames[key_frames.index(i)+1]), video_data[i]) for i in key_frames if i != video_length]
-#
-#     if os.path.exists(keyframe_path):
-#         os.system('cd {} && rm *'.format(keyframe_path))
-#     for i, keyframe in enumerate(keyframes_data):
-#         Image.fromarray(keyframe[1]).save(f"{keyframe_path}/{i}.jpeg")
-#
-#     return keyframes_data
+def get_keyframes_data(video_data: 'np.ndarray', cut_sim: float):
+    last_hash = imagehash.phash(Image.fromarray(video_data[0]))
+    key_frames = [0]
+    frame_num = 0
+    for each_frame in video_data:
+        frame_hash = imagehash.phash(Image.fromarray(each_frame))
+        similarity = 1 - (last_hash - frame_hash) / len(frame_hash.hash) ** 2
+        if similarity < cut_sim:
+            key_frames.append(frame_num)
+        frame_num += 1
+        last_hash = frame_hash
+    video_length = len(video_data)
+    key_frames.append(video_length)
+    keyframes_data = [((i, key_frames[key_frames.index(i)+1]), video_data[i]) for i in key_frames if i != video_length]
+    return keyframes_data
 
 
 def search_frame(keyframe_da: DocumentArray, prompt: str, topn: int, server_url: str, token: str):
     client = Client(server_url, credential={'Authorization': token})
     d = Document(text=prompt, matches=keyframe_da)
+    # print("input matches: ", d.matches)
+    # print(d.matches[0])
     r = client.rank([d], show_progress=True)
-    result = r['@m', ['id', 'blob', 'scores__clip_score__value']]
-    return [each[:topn] for each in result]
+    # print("output: ", r[0].matches[0].id)
+    result = r['@m']
+    return result
+
+
+def bytes_to_nparray(input: bytes) -> np.ndarray:
+    import numpy as np
+    from PIL import Image
+    # print(input)
+    return np.array(Image.open(input))
