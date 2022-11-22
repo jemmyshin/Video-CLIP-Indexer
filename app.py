8b7a48c2ce202c09555b789718a7f4808f41ada1
jemmyshin <jem.fu@jina.ai>
2022-11-22 17:48:09 +0800

    feat: add av dependency

--- app.py
+++ app.py
@@ -2,9 +2,7 @@ import streamlit as st
 from helper import search_frame, get_keyframes_data
 import os
 import shutil
-import skvideo
-os.system('which ffmpeg')
-skvideo.setFFmpegPath("/usr/local/bin")
+import av
 import skvideo.io
 from docarray import DocumentArray, Document
 
@@ -24,16 +22,32 @@ search_button = st.button('Search')
 if analysis_button:
     with st.spinner('Extracting key frames...'):
         os.makedirs('tmp_videos', exist_ok=True)
+        os.makedirs('tmp_key_frames', exist_ok=True)
         with open('tmp_videos/' + uploaded_file.name, 'wb') as f:
             f.write(uploaded_file.getvalue())
-        st.session_state.video_data = skvideo.io.vread('tmp_videos/' + uploaded_file.name, num_frames=num_frames)
-        shutil.rmtree('tmp_videos')
-        keyframe_data = get_keyframes_data(st.session_state.video_data, float(cut_sim_value))
+
+        keyframe_data = []
+        with av.open('tmp_videos/' + uploaded_file.name) as container:
+            # Signal that we only want to look at keyframes.
+            stream = container.streams.video[0]
+            stream.codec_context.skip_frame = 'NONKEY'
+            for frame in container.decode(stream):
+                frame.to_image().save(
+                    'tmp_key_frames/frame.{:04d}.png'.format(frame.pts),
+                    quality=80,
+                )
+                keyframe_data.append('tmp_key_frames/frame.{:04d}.png'.format(frame.pts))
+
+        # st.session_state.video_data = skvideo.io.vread('tmp_videos/' + uploaded_file.name, num_frames=num_frames)
+        # shutil.rmtree('tmp_videos')
+        # keyframe_data = get_keyframes_data(st.session_state.video_data, float(cut_sim_value))
         st.session_state.keyframe_data = DocumentArray([Document(tags={'left': str(tup[0][0]),
                                                                        'right': str(tup[0][1])},
-                                                                 tensor=tup[1]).convert_image_tensor_to_blob() for tup in keyframe_data])
-        for d in st.session_state.keyframe_data:
-            d.tensor = None
+                                                                 uri=tup).load_uri_to_blob() for tup in keyframe_data])
+        # for d in st.session_state.keyframe_data:
+        #     d.tensor = None
+        shutil.rmtree('tmp_videos')
+        shutil.rmtree('tmp_key_frames')
         st.success('Done extracting key frames, now you can search for it!')
 
 if search_button:
